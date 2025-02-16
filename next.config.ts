5a00442da72e8000054192e3a191ed66a8da9843
mohammadrezataghian <mohammadreza.taghian79@gmail.com>
2025-02-16 17:23:16 +0330

    implementing pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development',
+  disable: false,
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
